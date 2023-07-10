daa355dcb031621e9bf2ffc051d0645f41e128e8
Anderson <andersoncosta.si.som@gmail.com>
2023-07-10 15:03:56 -0300

    Update no UserService

--- src/main/java/com/anderson/demo/repositories/UserRepository.java
+++ src/main/java/com/anderson/demo/repositories/UserRepository.java
@@ -1,11 +1,10 @@
 package com.anderson.demo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.anderson.demo.models.User;
 
 // @Repository
-public interface UserRepositori extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     
 }
