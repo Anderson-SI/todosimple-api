6fd19fbe8e78b77520008f3c85d4a632753e5504
Anderson <andersoncosta.si.som@gmail.com>
2023-07-12 18:01:37 -0300

    Criacao do controller

--- src/main/java/com/anderson/demo/repositories/TaskRepository.java
+++ src/main/java/com/anderson/demo/repositories/TaskRepository.java
@@ -3,18 +3,18 @@ package com.anderson.demo.repositories;
 
 import java.util.List;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.stereotype.Repository;
+
 
 import com.anderson.demo.models.Task;
 
-// @Repository
-@NoRepositoryBean 
+//@Repository 
+//@NoRepositoryBean
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
-    
-
-    List<Task> FindByUser_Id(Long id);
+    //List<Task> FindByUser_Id(Long id);
     
     // @Query(value = "SELECT t FROM Task t WHERE t.user.id = :id")
     // List<Task> FindByUserId (@Param("user_id") Long id);
